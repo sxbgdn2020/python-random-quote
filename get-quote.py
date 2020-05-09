87cefb55c0e652629e6bdd0fb494539c526da6bc
sxbgdn2020 <65059236+sxbgdn2020@users.noreply.github.com>
2020-05-09 14:14:09 +0800

    Update get-quote.py
    czxczc

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,1 @@
-def main():
-  # print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
-
-if __name__== "__main__":
-  main()
